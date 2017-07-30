083f7994dcb69c234aeb7ddb40a296c6c740667a
Saroj Dahal <saroj.dahal.1213@gmail.com>
2017-07-30 16:39:37 +0545

    I have autoloaded library database in autoload.php and i have created model

---
+++ application/models/School_Model.php
@@ -0,0 +1,16 @@
+<?php
+
+public class School_Model extends CI_Model
+{
+	
+	function getData()
+	{
+	
+	$this->load->db('database');
+	$result = $this->db->query("select * from student");
+	return $result;
+	
+	}
+}
+
+?>
